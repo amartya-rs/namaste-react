475fad1838164d1243aa63f3191e67a4e6febd98
amartya-rs <saha.amartya@outlook.com>
2023-11-09 14:45:43 +0530

    feat: inception

---
+++ app.js
@@ -0,0 +1,21 @@
+/**
+ * Nested structure
+ */
+
+const parent = React.createElement(
+   "div",
+   { id: "parent" },
+   React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am h1"),
+      React.createElement("h1", {}, "I am h1 second"),
+   ])
+);
+
+const h1 = React.createElement(
+   "h1",
+   { className: "heading" },
+   "Namaste react!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
