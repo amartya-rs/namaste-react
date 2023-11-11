135fd09861af03adc0605f4c58eb6812c5eb3512
amartya-rs <saha.amartya@outlook.com>
2023-11-11 15:54:43 +0530

    feat: jsx

--- app.js
+++ app.js
@@ -5,13 +5,36 @@ import ReactDOM from "react-dom/client";
  * Nested structure
  */
 
-const parent = React.createElement(
-   "div",
-   { id: "parent" },
-   React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I am h1"),
-      React.createElement("h1", {}, "I am h1 second"),
-   ])
+// const parent = React.createElement(
+//    "div",
+//    { id: "parent" },
+//    React.createElement("div", { id: "child" }, [
+//       React.createElement("h1", {}, "I am h1"),
+//       React.createElement("h1", {}, "I am h1 second"),
+//    ])
+// );
+
+/** JSX */
+const parent = (
+   <div id="parent">
+      <div id="child">
+         <h1>I am h1</h1>
+         <h1> h1</h1>
+      </div>
+   </div>
+);
+
+const Title = () => {
+   return <h1>this is title</h1>;
+};
+
+/** Functional Component */
+const Parent = () => (
+   <div>
+      <Title />
+      <h1>this is h1 tag 1</h1>
+      <h1>this is h1 tag 2</h1>
+   </div>
 );
 
 const h1 = React.createElement(
@@ -19,6 +42,7 @@ const h1 = React.createElement(
    { className: "heading" },
    "Namaste react!"
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<Parent />);
